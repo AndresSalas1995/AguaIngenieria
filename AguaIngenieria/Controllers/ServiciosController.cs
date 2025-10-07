9ca8680a3aba1b21cbc1e114761a094191e6c8cf
WalyCR <andres.salas.quiros1995@gmail.com>
2025-10-07 08:49:17 -0600

    Se sube logo en formato png para mejor visual

---
+++ AguaIngenieria/Controllers/ServiciosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AguaIngenieria.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        // GET: Servicios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GetServicios Principal
+        public ActionResult Servicios()
+        {
+            return View();
+        }
+
+    }
+}
