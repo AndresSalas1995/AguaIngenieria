d4c30302ac52fa5de720e0436180bd9d2f165e5d
WalyCR <andres.salas.quiros1995@gmail.com>
2025-10-09 17:28:33 -0600

    Proyectos pagina finalizada, esperando feedback cliente y profesora - reunion proximo 10/10/25

---
+++ AguaIngenieria/Controllers/ProyectosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AguaIngenieria.Controllers
+{
+    public class ProyectosController : Controller
+    {
+        // GET: Proyectos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Proyectos
+        public ActionResult Proyectos()
+        {
+            return View();
+        }
+    }
+}
