8cffdbb6a813453f1a5fa964588a79838218c6a4
WalyCR <andres.salas.quiros1995@gmail.com>
2025-10-10 14:51:20 -0600

    Se empieza la seccion de contacto.

---
+++ AguaIngenieria/Controllers/ContactoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AguaIngenieria.Controllers
+{
+    public class ContactoController : Controller
+    {
+        // GET: Contacto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Get: Formulario de contacto
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        //GET: Ubicación
+        public ActionResult Ubicacion()
+        {
+            return View();
+        }
+    }
+}
